905985e0417d8fd0479987a9cadc048f944c8a6c
RGreenWolf <57684328+RGreenWolf@users.noreply.github.com>
2024-12-02 19:15:18 +0100

    Réorganisation du code : suppression de l'ancien fichier main.py, ajout d'un nouveau fichier main.py avec la gestion du serveur et du client, création de scripts de démarrage et ajout d'un fichier de dépendances.

---
+++ start.bat
@@ -0,0 +1,20 @@
+@echo off
+chcp 65001 > nul
+
+setlocal enabledelayedexpansion
+set DEPENDENCIES_FILE=dependances.txt
+set REQUIRED_LIBRARIES=
+for /f "tokens=*" %%i in (%DEPENDENCIES_FILE%) do (
+    set REQUIRED_LIBRARIES=!REQUIRED_LIBRARIES! %%i
+)
+for %%I in (%REQUIRED_LIBRARIES%) do (
+    pip show %%I > nul 2> nul
+    echo Vérification en cour de %%I...
+    if errorlevel 1 (
+        echo %%I n'est pas installé. Installation en cours...
+        pip install %%I
+    ) else (
+        echo %%I est déjà installé.
+    )
+)
+python ./src/main.py %*
