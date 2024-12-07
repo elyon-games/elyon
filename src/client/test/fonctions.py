b2b331e647c4f47edadd22c6e81276752ed90ebc
RGreenWolf <57684328+RGreenWolf@users.noreply.github.com>
2024-12-07 20:07:40 +0100

    sorry 😅

---
+++ src/client/test/fonctions.py
@@ -0,0 +1,40 @@
+import requests
+import json
+
+def login() -> None:
+    try:
+        vérif, data = json_existe()
+        print(f"vérif : {vérif}, data : {data}")
+        if vérif:
+            connexion_auto(data)
+        else:
+            create_json("test", "test")
+    except:
+        # récupérer dans l'interface name et password 
+        create_json("test", "test")
+
+
+def create_account() -> None:
+    print("create_account")
+
+def json_existe() -> bool:
+    try :
+        with open("data.json", "r+")as file :
+            data = json.load(file)
+        if data["Name"] and data["Password"]:
+            return True, data
+        else:
+            return False, 0
+    except:
+        return False, 0
+
+def connexion_auto(data) -> None:
+    print("connecter l'utilisateur")
+
+def create_json(name, password) -> None:
+    data = {"Name": name, "Password": password}
+    with open("data.json", "w+")as file :
+        json.dump(data, file)
+
+
+login()
---
+++ src/client/test/main.py
@@ -0,0 +1,85 @@
+import pygame 
+import pygame_widgets
+from pygame_widgets.textbox import TextBox
+from pygame_widgets.button import Button
+from jeu_dammier.main import main as dammier
+from nombre_mystère.nombre_mystère import menu as nb_mystère
+from function_menu import *
+
+def menu(screen, font)-> dict:
+    menu={
+        "titre": font.render(
+            "Mon titre",
+              True, 
+              (255, 255, 255)
+            ), "bouton":{
+                "bouton_dammier": Button(
+                    screen, 
+                    0,
+                    100,
+                    100,
+                    100, 
+                    text='dammier',
+                    fontSize=40,
+                    onClick=lambda: dammier()
+                ), 
+                "bouton_nb_mystère": Button(
+                    screen,
+                    100,
+                    100,
+                    100,
+                    100,
+                    text="nomber mystère",
+                    onClick=lambda: nb_mystère()
+                ), "bouton_pendu": Button(
+                    screen,
+                    200,
+                    100,
+                    100,
+                    100,
+                    text="pendu",
+                    onClick=pendu
+                )
+            }
+    }
+    
+    return menu
+
+def pendu():
+    print("pendu")
+
+def Main()-> None:
+    pygame.init()
+    font = pygame.font.Font(None, 80)
+    screen = pygame.display.set_mode((700,700))
+    screen_menu = menu(screen, font)
+    print(screen_menu)
+    
+    running = True
+    clock = pygame.time.Clock()
+    while running:
+        screen.fill((0,0,0))
+        events=pygame.event.get()
+        for event in events:
+            if event.type == pygame.QUIT:
+                running = False
+
+        for i in screen_menu.keys():
+            if i != "bouton":
+                screen.blit(screen_menu[i], screen_menu[i].get_rect(center=(0,0)))
+            else :
+                for y in screen_menu[i].values():
+                    print("y :",y)
+                    y.draw()
+
+        pygame_widgets.update(events)
+        
+        for i in screen_menu:
+            print(i)
+
+
+        pygame.display.update()
+        clock.tick(60)
+
+    pygame.quit()
+Main()
