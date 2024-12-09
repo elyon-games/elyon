b69a35a1ba167cc951f43d1fce22a3070c8b13eb
RGreenWolf <57684328+RGreenWolf@users.noreply.github.com>
2024-12-09 09:34:48 +0100

    Mise à jour du script de construction pour utiliser pyinstaller directement. Modification des configurations de serveur dans client.yaml pour pointer vers le serveur de production. Ajout de nouvelles dépendances dans dependances.txt.

--- build.sh
+++ build.sh
@@ -12,8 +12,5 @@ rm -f __main__.spec
 rm -f dist/__main__.exe
 rm -rf build
 
-# Définir le chemin vers pyinstaller
-pyinstaller_path="$HOME/.local/bin/pyinstaller"
-
 # Exécuter pyinstaller avec les options spécifiées
-"$pyinstaller_path" --add-data "$config_path:config" --add-data "$public_server:server_public_files" --add-data "$assets_path:assets" --onefile "$src_path" --icon="$assets_path/logo/round.ico"
+pyinstaller --add-data "$config_path:config" --add-data "$public_server:server_public_files" --add-data "$assets_path:assets" --onefile "$src_path" --icon="$assets_path/logo/round.ico"
