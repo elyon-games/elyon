997108cdeb83510dd6f71ee1e29b18c4ece3d789
RGreenWolf <57684328+RGreenWolf@users.noreply.github.com>
2025-01-06 19:30:48 +0100

    Amélioration de l'interface utilisateur et mise à jour de la logique de connexion : ajustement des dimensions des composants, ajout de la gestion de l'état en ligne/hors ligne, et optimisation des appels de serveur.

--- src/main.py
+++ src/main.py
@@ -9,6 +9,11 @@ import common.path as path
 import common.data as data
 import common.assets as assets
 import common.logger as Logger
+import customtkinter as ctk
+import webbrowser
+
+online = False
+server_host = False
 
 def start_server() -> None:
     if args.asArg("clear-data") and args.getArg("clear-data") in ["server", "all"]:
@@ -19,30 +24,22 @@ def start_server() -> None:
     Server.Main(config=config_server, options=options)
 
 def start_client() -> None:
-    global server_host
+    global online
     if args.asArg("clear-data") and args.getArg("clear-data") in ["client", "all"]:
         data.clearClientData()
-    data.createClientData()        
+    data.createClientData()
     import client.main as Client
     config_client = config.getConfig("client", configMode)
-    config_client.update({
-        "online": online,
-    })
-    if not online and server_host is not False:
-        config_client.update({
-            "server": {
-                "host": server_host,
-            }
-        })
+    config_client.update({"online": online})
+    if not online and server_host:
+        config_client.update({"server": {"host": server_host}})
     Client.Main(config=config_client, options=options)
 
 def start_local() -> None:
     server_thread = threading.Thread(target=start_server, name="ServerThread", daemon=True)
     client_thread = threading.Thread(target=start_client, name="ClientThread", daemon=True)
-
     server_thread.start()
     client_thread.start()
-
     client_thread.join()
     print("Le client a terminé.")
 
@@ -50,7 +47,7 @@ def start_GUI() -> None:
     COLOR_PRIMARY = "#10B981"
     COLOR_SECONDARY = "#2563EB"
     COLOR_TEXT = "#2E2E2E"
-    FONT_TITLE = ("Arial", 16)
+    FONT_TITLE = ("Arial", 24)
     FONT_FOOTER = ("Arial", 10, "bold")
     BUTTON_HEIGHT = 50
     BUTTON_WIDTH = 200
@@ -58,16 +55,12 @@ def start_GUI() -> None:
     ENTRY_WIDTH = 300
     FOOTER_HEIGHT = 50
 
-    import sys
-    import customtkinter as ctk
-
     def on_configure_server_click():
-        global server_host
-        global online
+        global server_host, online
         ip = ip_entry.get()
         if ip:
             try:
-                res = requests.get(f"{ip}/api/client/info")
+                res = requests.get(f"http://{ip}/api/client/info")
                 if res.status_code == 200:
                     server_host = ip
                     online = False
@@ -98,96 +91,58 @@ def start_GUI() -> None:
         sys.exit()
 
     def open_github():
-        import webbrowser
         webbrowser.open("https://github.com/elyon-games/")
 
     def open_website():
-        import webbrowser
         webbrowser.open("https://elyon.younity-mc.fr")
 
     app = ctk.CTk()
-    app.title("Elyon - Menu principal")
-    app.geometry("400x300")
+    app.title("Elyon Launcher")
+    app.geometry("400x350")
     app.iconbitmap(assets.getAsset("/logo/round.ico"))
     app.protocol("WM_DELETE_WINDOW", on_close)
     app.resizable(False, False)
 
-    ctk.CTkLabel(app, text="Bienvenue sur Elyon", font=("Arial", 20, "bold")).pack(pady=2)
+    ctk.CTkLabel(app, text="Bienvenue sur Elyon", font=("Arial", 28, "bold")).pack(pady=5)
 
     tabview = ctk.CTkTabview(app)
     tabview.pack(expand=True, fill="both", padx=10)
 
     tab_public = tabview.add("Serveur Public")
-    public_label = ctk.CTkLabel(tab_public, text="Démarrer un serveur public", font=FONT_TITLE)
-    public_label.pack(pady=10)
-    public_button = ctk.CTkButton(
-        tab_public, text="lancer", command=on_start_client, 
-        font=FONT_TITLE,
-        height=BUTTON_HEIGHT, width=BUTTON_WIDTH, fg_color=COLOR_PRIMARY
-    )
-    public_button.pack(pady=20)
+    ctk.CTkLabel(tab_public, text="Démarrer un serveur public", font=FONT_TITLE).pack(pady=10)
+    ctk.CTkButton(tab_public, text="lancer", command=on_start_client, font=FONT_TITLE,
+                  height=BUTTON_HEIGHT, width=BUTTON_WIDTH, fg_color=COLOR_PRIMARY).pack(pady=20)
 
     tab_private = tabview.add("Serveur Privé")
-    private_label = ctk.CTkLabel(tab_private, text="Configurer le serveur privé", font=FONT_TITLE)
-    private_label.pack(pady=2)
-
-    ip_entry = ctk.CTkEntry(
-        tab_private, placeholder_text="Entrez l'adresse IP du serveur", 
-        height=ENTRY_HEIGHT, width=ENTRY_WIDTH
-    )
+    ctk.CTkLabel(tab_private, text="Configurer le serveur privé", font=FONT_TITLE).pack(pady=2)
+    ip_entry = ctk.CTkEntry(tab_private, placeholder_text="Entrez l'adresse IP du serveur",
+                            height=ENTRY_HEIGHT, width=ENTRY_WIDTH)
     ip_entry.pack(pady=2)
-
-    configure_button = ctk.CTkButton(
-        tab_private, text="Configurer", command=on_configure_server_click, 
-        height=BUTTON_HEIGHT, width=BUTTON_WIDTH, fg_color=COLOR_PRIMARY
-    )
+    configure_button = ctk.CTkButton(tab_private, text="Configurer", command=on_configure_server_click,
+                                     height=BUTTON_HEIGHT, width=BUTTON_WIDTH, fg_color=COLOR_PRIMARY)
     configure_button.pack(pady=2)
-
     status_label = ctk.CTkLabel(tab_private, text="")
     status_label.pack(pady=(5, 5))
 
     tab_offline = tabview.add("Mode Offline")
-    offline_label = ctk.CTkLabel(tab_offline, text="Jouer en mode hors ligne", font=FONT_TITLE)
-    offline_label.pack(pady=10)
-    offline_button = ctk.CTkButton(
-        tab_offline, text="Démarrer", command=on_start_local, 
-        height=BUTTON_HEIGHT, width=BUTTON_WIDTH, fg_color=COLOR_PRIMARY
-    )
-    offline_button.pack(pady=20)
+    ctk.CTkLabel(tab_offline, text="Jouer en mode hors ligne", font=FONT_TITLE).pack(pady=10)
+    ctk.CTkButton(tab_offline, text="Démarrer", command=on_start_local,
+                  height=BUTTON_HEIGHT, width=BUTTON_WIDTH, fg_color=COLOR_PRIMARY).pack(pady=20)
 
     footer_frame = ctk.CTkFrame(app, fg_color=COLOR_PRIMARY, height=FOOTER_HEIGHT)
     footer_frame.pack(side="bottom", fill="x")
-
-    copyright_label = ctk.CTkLabel(
-        footer_frame, text="© 2024 Elyon Games. Tous droits réservés.", 
-        font=FONT_FOOTER, text_color=COLOR_TEXT
-    )
-    copyright_label.pack(side="left", padx=10)
-
-    github_icon = ctk.CTkButton(
-        footer_frame, text="GitHub", command=open_github, 
-        height=30, width=80, fg_color=COLOR_SECONDARY
-    )
-    github_icon.pack(side="right", padx=5, pady=5)
-
-    website_icon = ctk.CTkButton(
-        footer_frame, text="Site Web", command=open_website, 
-        height=30, width=80, fg_color=COLOR_SECONDARY
-    )
-    website_icon.pack(side="right", padx=5, pady=5)
+    ctk.CTkLabel(footer_frame, text="© 2024 Elyon Games. Tous droits réservés.",
+                 font=FONT_FOOTER, text_color=COLOR_TEXT).pack(side="left", padx=5)
+    ctk.CTkButton(footer_frame, text="GitHub", command=open_github,
+                  height=30, width=80, fg_color=COLOR_SECONDARY).pack(side="right", padx=2, pady=0)
+    ctk.CTkButton(footer_frame, text="Site Web", command=open_website,
+                  height=30, width=80, fg_color=COLOR_SECONDARY).pack(side="right", padx=2, pady=0)
 
     app.mainloop()
 
-
 def Main() -> None:
-    global options
-    global mode
-    global configMode
-    global type
-    global logger
-    global server_host
+    global options, mode, configMode, type, logger, server_host, online
 
-    server_host = False
     try:
         options = args.get_format_args()
         mode = utils.getMode()
@@ -195,7 +150,6 @@ def Main() -> None:
         type = args.getArg("type") if args.asArg("type") else "gui"
 
         path.initPath(options.get("data-path") if options.get("data-path") else "./data")
-
         data.createDataFolder()
 
         logger = Logger.LoggerManager(path.get_path("logs"))
