512c4746b6ae61793d413fcd38135d8edcd4924f
tEtEmElE <theophile.melerowicz@gmail.com>
2025-01-14 19:01:52 +0100

    découpage en fonction est changemetn des pack en grid

--- src/main.py
+++ src/main.py
@@ -64,7 +64,7 @@ def ping_server(ip):
     except requests.exceptions.RequestException:
         return False
 
-def start_GUI() -> None:
+def start_GUI():
     COLOR_PRIMARY = "#10B981"
     COLOR_SECONDARY = "#2563EB"
     COLOR_TEXT = "#2E2E2E"
@@ -83,7 +83,7 @@ def start_GUI() -> None:
         app.destroy()
         start_client()
 
-    def on_configure_server_click():
+    def on_configure_server_click(saved_servers_inner_frame, status_label, ip_entry):
         global server_host, online
         ip = ip_entry.get()
         if ip:
@@ -93,13 +93,13 @@ def start_GUI() -> None:
                 status_label.configure(text="Adresse IP valide!", text_color="green")
                 ip_entry.delete(0, 'end')
                 save_server(ip)
-                update_saved_servers()
+                update_saved_servers(saved_servers_inner_frame, status_label)
             else:
                 status_label.configure(text="Le serveur n'est pas accessible.", text_color="red")
         else:
             status_label.configure(text="Veuillez entrer une adresse IP valide.", text_color="red")
 
-    def on_connect_to_server(ip):
+    def on_connect_to_server(ip, status_label):
         global server_host, online
         if ping_server(ip):
             server_host = ip
@@ -109,18 +109,18 @@ def start_GUI() -> None:
         else:
             status_label.configure(text="Le serveur n'est pas accessible.", text_color="red")
 
-    def update_saved_servers():
+    def update_saved_servers(saved_servers_inner_frame, status_label):
         for widget in saved_servers_inner_frame.winfo_children():
             widget.destroy()
         servers = load_saved_servers()
         for server in servers:
             status = "En ligne" if ping_server(server) else "Hors ligne"
             server_frame = ctk.CTkFrame(saved_servers_inner_frame, fg_color="white")
-            server_frame.pack(pady=2, fill="x")
-            ctk.CTkLabel(server_frame, text=f"{server} - {status}", fg_color="white").pack(side="left", padx=5)
+            server_frame.grid(pady=2, sticky="ew")
+            ctk.CTkLabel(server_frame, text=f"{server} - {status}", fg_color="white").grid(row=0, column=0, padx=5)
             if status == "En ligne":
-                ctk.CTkButton(server_frame, text="Se connecter", command=lambda ip=server: on_connect_to_server(ip),
-                              height=30, width=100, fg_color=COLOR_SECONDARY).pack(side="right", padx=5)
+                ctk.CTkButton(server_frame, text="Se connecter", command=lambda ip=server: on_connect_to_server(ip, status_label),
+                              height=30, width=100, fg_color=COLOR_SECONDARY).grid(row=0, column=1, padx=5)
 
     def on_start_local():
         global online
@@ -135,106 +135,101 @@ def start_GUI() -> None:
     def open_website():
         webbrowser.open("https://elyon.younity-mc.fr")
 
+    def Serveur_Officiel(tabview):
+                # Official server tab
+        ctk.CTkLabel(tab_official, text="Se connecter au serveur officiel", font=FONT_TITLE).grid(pady=10, row=0, column=1, sticky="e")
+        ctk.CTkButton(tab_official, text="Se connecter", command=on_connect_to_official_server, font=FONT_TITLE,
+                    height=BUTTON_HEIGHT, width=BUTTON_WIDTH, fg_color=COLOR_PRIMARY).grid(pady=20, row=1, column=1, sticky="e")
+
+    def Serveur_Privat(tabview): 
+        # Private servers tab
+        ctk.CTkLabel(tab_private, text="Configurer un serveur privé", font=FONT_TITLE).grid(pady=2, row=0, column=0)
+        ip_entry = ctk.CTkEntry(tab_private, placeholder_text="Entrez l'adresse IP du serveur",
+                                height=ENTRY_HEIGHT, width=ENTRY_WIDTH)
+        ip_entry.grid(pady=2, row=1, column=0)
+        configure_button = ctk.CTkButton(tab_private, text="Ajouter", command=lambda : on_configure_server_click(saved_servers_inner_frame, status_label, ip_entry),
+                                        height=BUTTON_HEIGHT, width=BUTTON_WIDTH, fg_color=COLOR_PRIMARY)
+        configure_button.grid(pady=2, row=2, column=0)
+        status_label = ctk.CTkLabel(tab_private, text="")
+        status_label.grid(pady=(5, 5), row=3, column=0)
+
+        saved_servers_frame = ctk.CTkFrame(tab_private, fg_color="white")
+        saved_servers_frame.grid(pady=10, sticky="nsew", row=4, column=0)
+
+        saved_servers_frame.grid_rowconfigure(0, weight=1)
+        saved_servers_frame.grid_columnconfigure(0, weight=1)
+
+        saved_servers_canvas = ctk.CTkCanvas(saved_servers_frame, bg="white")
+        saved_servers_canvas.grid(sticky="nsew", row=0, column=0)
+
+        scrollbar = ctk.CTkScrollbar(saved_servers_frame, command=saved_servers_canvas.yview)
+        scrollbar.grid(sticky="ns", row=0, column=1)
+
+        saved_servers_canvas.configure(yscrollcommand=scrollbar.set)
+        saved_servers_canvas.bind('<Configure>', lambda e: saved_servers_canvas.configure(scrollregion=saved_servers_canvas.bbox("all")))
+
+        saved_servers_inner_frame = ctk.CTkFrame(saved_servers_canvas, fg_color="white", bg_color="cyan")
+        saved_servers_canvas.create_window((0, 0), window=saved_servers_inner_frame, anchor="nw")
+
+        update_saved_servers(saved_servers_inner_frame, status_label)
+
+        ctk.CTkButton(tab_private, text="Actualiser", command=lambda: update_saved_servers(saved_servers_inner_frame, status_label),
+                    height=BUTTON_HEIGHT, width=BUTTON_WIDTH, fg_color=COLOR_PRIMARY).grid(pady=10, row=5, column=0)
+
     app = ctk.CTk()
+    
+    def Offline(tabview):
+        # Offline mode tab
+        ctk.CTkLabel(tab_offline, text="Jouer en mode hors ligne", font=FONT_TITLE).grid(pady=10, row=0, column=0)
+        ctk.CTkButton(tab_offline, text="Démarrer", command=on_start_local,
+                    height=BUTTON_HEIGHT, width=BUTTON_WIDTH, fg_color=COLOR_PRIMARY).grid(pady=20, row=1, column=0)
+
+    def footer():
+        footer_frame = ctk.CTkFrame(app, fg_color=COLOR_PRIMARY, height=FOOTER_HEIGHT)
+        footer_frame.grid(sticky="ew", row=2, column=0, columnspan=20)
+
+        footer_frame.grid_columnconfigure(0, weight=1)  # Left side of the footer
+        footer_frame.grid_columnconfigure(1, weight=0)  # Right side of the footer
+
+        ctk.CTkLabel(footer_frame, text="© 2024 Elyon Games. Tous droits réservés.",
+                    font=FONT_FOOTER, text_color=COLOR_TEXT).grid(padx=5, pady=0, row=0, column=0, sticky="w")
+
+        ctk.CTkButton(footer_frame, text="Site Web", command=open_website,
+                    height=30, width=80, fg_color=COLOR_SECONDARY).grid(padx=5, pady=0, row=0, column=1, sticky="e")
+
+
     app.title("Elyon Games Launcher")
-    app.geometry("400x500")
+    app.geometry("600x500")
     app.iconbitmap(assets.getAsset("/logo/round.ico"))
     app.protocol("WM_DELETE_WINDOW", on_close)
-    app.resizable(False, False)
+    app.resizable(True, True)
+    ctk.set_appearance_mode("dark")
 
-    ctk.CTkLabel(app, text="Bienvenue sur Elyon Games Launcher", font=("Arial", 28, "bold")).pack(pady=5)
+    ctk.CTkLabel(app, text="Bienvenue sur Elyon Games Launcher", font=("Arial", 28, "bold")).grid(pady=5, row=0, column=0, columnspan=20)
+
+    # Configure main grid for dynamic resizing
+    app.grid_rowconfigure(1, weight=1)
+    for i in range(20):
+        app.grid_columnconfigure(i, weight=1)
 
     tabview = ctk.CTkTabview(app)
-    tabview.pack(expand=True, fill="both", padx=10)
+    tabview.grid(sticky="nsew", padx=10, row=1, column=0, columnspan=20)
+
 
     tab_official = tabview.add("Serveur Officiel")
-    ctk.CTkLabel(tab_official, text="Se connecter au serveur officiel", font=FONT_TITLE).pack(pady=10)
-    ctk.CTkButton(tab_official, text="Se connecter", command=on_connect_to_official_server, font=FONT_TITLE,
-                  height=BUTTON_HEIGHT, width=BUTTON_WIDTH, fg_color=COLOR_PRIMARY).pack(pady=20)
-
     tab_private = tabview.add("Serveurs Privés")
-    ctk.CTkLabel(tab_private, text="Configurer un serveur privé", font=FONT_TITLE).pack(pady=2)
-    ip_entry = ctk.CTkEntry(tab_private, placeholder_text="Entrez l'adresse IP du serveur",
-                            height=ENTRY_HEIGHT, width=ENTRY_WIDTH)
-    ip_entry.pack(pady=2)
-    configure_button = ctk.CTkButton(tab_private, text="Ajouter", command=on_configure_server_click,
-                                     height=BUTTON_HEIGHT, width=BUTTON_WIDTH, fg_color=COLOR_PRIMARY)
-    configure_button.pack(pady=2)
-    status_label = ctk.CTkLabel(tab_private, text="")
-    status_label.pack(pady=(5, 5))
-
-    saved_servers_frame = ctk.CTkFrame(tab_private, fg_color="white")
-    saved_servers_frame.pack(pady=10, fill="both", expand=True)
-
-    saved_servers_canvas = ctk.CTkCanvas(saved_servers_frame, bg="white")
-    saved_servers_canvas.pack(side="left", fill="both", expand=True)
-
-    scrollbar = ctk.CTkScrollbar(saved_servers_frame, command=saved_servers_canvas.yview)
-    scrollbar.pack(side="right", fill="y")
-
-    saved_servers_canvas.configure(yscrollcommand=scrollbar.set)
-    saved_servers_canvas.bind('<Configure>', lambda e: saved_servers_canvas.configure(scrollregion=saved_servers_canvas.bbox("all")))
-
-    saved_servers_inner_frame = ctk.CTkFrame(saved_servers_canvas, fg_color="white", bg_color="cyan")
-    saved_servers_canvas.create_window((0, 0), window=saved_servers_inner_frame, anchor="nw")
-
-    update_saved_servers()
-
-    ctk.CTkButton(tab_private, text="Actualiser", command=update_saved_servers,
-                  height=BUTTON_HEIGHT, width=BUTTON_WIDTH, fg_color=COLOR_PRIMARY).pack(pady=10)
-
     tab_offline = tabview.add("Mode Offline")
-    ctk.CTkLabel(tab_offline, text="Jouer en mode hors ligne", font=FONT_TITLE).pack(pady=10)
-    ctk.CTkButton(tab_offline, text="Démarrer", command=on_start_local,
-                  height=BUTTON_HEIGHT, width=BUTTON_WIDTH, fg_color=COLOR_PRIMARY).pack(pady=20)
 
-    footer_frame = ctk.CTkFrame(app, fg_color=COLOR_PRIMARY, height=FOOTER_HEIGHT)
-    footer_frame.pack(side="bottom", fill="x")
-    ctk.CTkLabel(footer_frame, text="© 2024 Elyon Games. Tous droits réservés.",
-                 font=FONT_FOOTER, text_color=COLOR_TEXT).pack(side="left", padx=5)
-    ctk.CTkButton(footer_frame, text="Site Web", command=open_website,
-                  height=30, width=80, fg_color=COLOR_SECONDARY).pack(side="right", padx=2, pady=0)
+    Serveur_Officiel(tab_official)
 
-    app.mainloop()
+    Serveur_Privat(tab_private)
 
-def Main() -> None:
-    global options, mode, configMode, type, logger, server_host, online
+    Offline(tab_offline)
 
-    try:
-        options = args.get_format_args()
-        mode = utils.getMode()
-        configMode = args.getArg("config") if args.asArg("config") else utils.getMode()
-        type = args.getArg("type") if args.asArg("type") else "gui"
+    footer()
 
-        path.initPath(options.get("data-path") if options.get("data-path") else "./data")
-        data.createDataFolder()
-
-        logger = Logger.LoggerManager(path.get_path("logs"))
-        logger(f"Logger start")
-
-        if utils.getDevModeStatus():
-            print(f"Options : {options}")
-        print(f"Mode : {mode}")
-        print(f"Type : {type}")
-        print(f"Config : {configMode}")
-
-        if type == "gui":
-            start_GUI()
-        elif type == "server":
-            start_server()
-        elif type == "client":
-            start_client()
-        elif type == "local":
-            start_local()
-        else:
-            raise ValueError("Type invalide. Veuillez choisir 'server', 'client', 'local' ou 'gui'")
-    except ValueError as e:
-        print(f"Erreur : {e}")
-    except IndexError:
-        print("Erreur : L'argument '--type' doit être suivi d'une valeur (server, client, local, gui)")
-    except Exception as e:
-        print(f"Une erreur imprévue est survenue : {e}")
-        traceback.print_exc()
+    return app
 
 if __name__ == "__main__":
-    Main()
+    app = start_GUI()
+    app.mainloop()
