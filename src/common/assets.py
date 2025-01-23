fce13f169e038df64cd17d5d6e773e96167a3e64
RGreenWolf <57684328+RGreenWolf@users.noreply.github.com>
2025-01-23 17:10:49 +0100

    Ajout de la fonction getAssetContent pour lire le contenu des fichiers d'actifs.

--- src/common/assets.py
+++ src/common/assets.py
@@ -5,3 +5,7 @@ def getAsset(relative_path: str) -> str:
     cleaned_path = relative_path.lstrip("/").replace("/", os.sep)
     base_path = os.path.join(sys._MEIPASS, "assets") if hasattr(sys, "_MEIPASS") else os.path.join(os.path.abspath("."), "assets")
     return os.path.join(base_path, cleaned_path)
+
+def getAssetContent(relative_path: str) -> str:
+    with open(getAsset(relative_path), "r") as file:
+        return file.read()
